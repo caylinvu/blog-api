4967cd4fd305243f4ee15a6b4213c351e61b9608
Caylin Vu <caylinvu@gmail.com>
2023-11-02 16:39:45 -0500

    Edit models and create route files

--- models/comment.js
+++ models/comment.js
@@ -6,11 +6,11 @@ const CommentSchema = new Schema({
   text: { type: String, required: true },
   timestamp: { type: Date, required: true },
   display_name: { type: String, required: true },
-  post_id: { type: Schema.Types.ObjectId, ref: 'Post' },
+  post: { type: Schema.Types.ObjectId, ref: 'Post', required: true },
 });
 
 // CommentSchema.virtual('formatted_timestamp').get(function () {
 
 // });
 
-module.exports = mongoose.model('User', CommentSchema);
+module.exports = mongoose.model('Comment', CommentSchema);
--- models/post.js
+++ models/post.js
@@ -6,7 +6,7 @@ const PostSchema = new Schema({
   title: { type: String, required: true },
   text: { type: String, required: true },
   timestamp: { type: Date, required: true },
-  author_id: { type: Schema.Types.ObjectId, ref: 'User' },
+  author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   isPublished: { type: Boolean, required: true },
 });
 
@@ -14,4 +14,4 @@ const PostSchema = new Schema({
 
 // });
 
-module.exports = mongoose.model('User', PostSchema);
+module.exports = mongoose.model('Post', PostSchema);
